25bf239c5646b0854a9d0bc758b504a8e7dc2f42
Andre530 <dallgnolfaustinoa@gmail.com>
2024-10-09 17:18:25 -0300

    ;

--- pong.js
+++ pong.js
@@ -27,33 +27,6 @@ document.addEventListener("keydown", () => {
         raqueteDirt.style.top = posicao + "vh";
     }
 })
-const movable = document.getElementById('movable');
-let posX = window.innerWidth / 2; // Posição inicial
-let posY = window.innerHeight / 2; // Posição inicial
-let speedX = 2; // Velocidade horizontal
-let speedY = 2; // Velocidade vertical
 
-function move() {
-    posX += speedX;
-    posY += speedY;
-
-    // Limitação da tela
-    const elementWidth = movable.offsetWidth;
-    const elementHeight = movable.offsetHeight;
-
-    // Verifica colisão com as bordas
-    if (posX + elementWidth > window.innerWidth || posX < 0) {
-        speedX = -speedX; // Inverte a direção horizontal
-    }
-    if (posY + elementHeight > window.innerHeight || posY < 0) {
-        speedY = -speedY; // Inverte a direção vertical
-    }
-
-    // Atualiza a posição do elemento
-    movable.style.left = `${posX}px`;
-    movable.style.top = `${posY}px`;
-
-    requestAnimationFrame(move); // Chama a função novamente
-}
 
 
